010834b9b61ac17dd23fef209a27c0a792eb006c
KremerMichael <kremeremichael@gmail.com>
2020-10-01 09:52:53 -0400

    auto push run at Thu 01 Oct 2020 09:52:53 AM EDT

---
+++ Model/model.py
@@ -0,0 +1,16 @@
+class Battery:
+
+    #Battery should keep track of it's internal charge, (colombs? kWh?)
+    charge = 0
+
+    #Function to charge battery based on some formula
+    def addCharge(self):
+        self.charge += 0 #some formula for charing here
+        #should this return the estimated cost of this charge?
+
+    #Function to drain battery based on some formula?
+    def drainCharge(self):
+        self.charge -= 0 #some formula for depletion here
+
+    
+        
